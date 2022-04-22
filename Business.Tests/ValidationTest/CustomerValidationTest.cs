db520be192b19c0e4ebfc6850630acb6dbee5135
uludagsercan <sercan.uludag91@hotmail.com>
2022-04-23 02:15:39 +0300

    Business Unit Test

---
+++ Business.Tests/ValidationTest/CustomerValidationTest.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+
+namespace Business.Tests.ValidationTest
+{
+    [TestClass]
+    public class CustomerValidationTest
+    {
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void Customer_validation_tests()
+        {
+            Mock<ICustomerDal> mock = new Mock<ICustomerDal>();
+            CustomerManager customerManager = new CustomerManager(mock.Object);
+            customerManager.Add(new Customer{ CustomerName="Sercan"});
+        }
+    }
+}
